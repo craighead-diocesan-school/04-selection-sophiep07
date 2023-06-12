f04ac41c3260c4cfa56f7e6f921bb7e3f2989612
sophiep07 <parsonso14@craighead.school.nz>
2023-06-13 10:59:44 +1200

    modified conversation

--- code/2-conversation.js
+++ code/2-conversation.js
@@ -5,16 +5,27 @@
 function conversation() {
   alert('Welcome to my conversation program.')
 
-  alert('Do you like cycling? Answer yes or no.')
-  let answer = prompt()
+  let answer = prompt('Do you like reading? Answer yes or no.')
 
   if (answer == 'yes') {
-    alert("That's good - you will get very fit.")
+    alert("That's good - you will learn many things.")
+    let secondQuestion = prompt('Do you like the movie adaptations of the books? Answer yes or no.')
+    if (secondQuestion == 'yes') {
+      alert('Cool! Watching movies after reading the books is fun.')
+    } else {
+      alert('That is understandable.')
+    }
   } else {
-    alert('Perhaps you like some other sport.')
+    alert('Perhaps you like movies instead.')
+    let thirdQuestion = prompt('Do you watch series too? Answer yes or no.')
+    if (thirdQuestion == 'yes') {
+      alert('Great! I love watching series too!')
+    } else {
+    alert("That's too bad.")
   }
 
   alert('Goodbye')
+  }
 }
 
 function age() {
