405422e2000e4ccaad0678bcde97e9f03b1476f2
sophiep07 <parsonso14@craighead.school.nz>
2023-06-17 11:51:14 +1200

    finished sale calculator

--- code/5-sale-calculator.js
+++ code/5-sale-calculator.js
@@ -3,5 +3,17 @@
 // ####################################
 
 function saleCalculator() {
-  // write your code here
+  let money = Number(prompt('How much money do you have?'))
+  let item = Number(prompt('How much is the item normally?'))
+  let percent = Number(prompt('What is the discount percentage?'))
+
+  let discount = percent / 100
+  let minus = item * discount
+  let cost = item - minus
+
+  if (cost > money) {
+    alert('You do not have enough money. You have ' + money + ' to spend, but even with ' + percent + '% off, $' + item + ' only comes down to $' + cost + '.')
+  } else {
+    alert('You can buy that item!')
+  }
 }
