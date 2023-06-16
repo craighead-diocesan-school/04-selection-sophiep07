331a653d3d500de9874b749688b53cf48d1fa5e7
sophiep07 <parsonso14@craighead.school.nz>
2023-06-17 11:52:02 +1200

    Update 3-more-than-two.js

--- code/3-more-than-two.js
+++ code/3-more-than-two.js
@@ -9,21 +9,21 @@ function moreThanTwo() {
 
   if (answer > 2) {
     alert(answer + ' is greater that two.')
-    let question = prompt('Do you want to go again? Answer yes or no.')
+    // let question = prompt('Do you want to go again? Answer yes or no.')
   } else {
     alert(answer + ' is less that two.')
-    let question = prompt('Do you want to go again? Answer yes or no.')
+    // let question = prompt('Do you want to go again? Answer yes or no.')
   } 
 
-  while (question = yes) {
-    let answer = Number(prompt('What number do you want to compare to two?'))
+  // while (question = yes) {
+  //   let answer = Number(prompt('What number do you want to compare to two?'))
 
-    if (answer > 2) {
-      alert(answer + ' is greater that two.')
-      let question = prompt('Do you want to go again? Answer yes or no.')
-    } else {
-      alert(answer + ' is less that two.')
-      let question = prompt('Do you want to go again? Answer yes or no.')
-  } 
-  }
+  //   if (answer > 2) {
+  //     alert(answer + ' is greater that two.')
+  //     let question = prompt('Do you want to go again? Answer yes or no.')
+  //   } else {
+  //     alert(answer + ' is less that two.')
+  //     let question = prompt('Do you want to go again? Answer yes or no.')
+  // } 
+  // }
 }
