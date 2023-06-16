09dcdbbf3fcb1f64c6f6f2ebb756f4aed2d77b6f
sophiep07 <parsonso14@craighead.school.nz>
2023-06-17 11:40:23 +1200

    finished capital

--- code/3-more-than-two.js
+++ code/3-more-than-two.js
@@ -4,4 +4,26 @@
 
 function moreThanTwo() {
   // write your code here
+  
+  let answer = Number(prompt('What number do you want to compare to two?'))
+
+  if (answer > 2) {
+    alert(answer + ' is greater that two.')
+    let question = prompt('Do you want to go again? Answer yes or no.')
+  } else {
+    alert(answer + ' is less that two.')
+    let question = prompt('Do you want to go again? Answer yes or no.')
+  } 
+
+  while (question = yes) {
+    let answer = Number(prompt('What number do you want to compare to two?'))
+
+    if (answer > 2) {
+      alert(answer + ' is greater that two.')
+      let question = prompt('Do you want to go again? Answer yes or no.')
+    } else {
+      alert(answer + ' is less that two.')
+      let question = prompt('Do you want to go again? Answer yes or no.')
+  } 
+  }
 }
--- code/4-capital.js
+++ code/4-capital.js
@@ -3,5 +3,13 @@
 // ####################################
 
 function capital() {
-  // write your code here
+  let name = prompt('What is your name?')
+
+  let capital = prompt ('Quick question, ' + name + '. What is the capital of France? Capitals matter.')
+
+  if (capital == 'Paris') {
+    alert('You are correct, ' + name + '.')
+  } else {
+    alert('You are wrong, ' + name + '.')
+  }
 }
